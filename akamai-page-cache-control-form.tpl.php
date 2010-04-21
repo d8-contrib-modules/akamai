265e4f338f44c27fe2745c6902b0402d95ee720f
Frank Febbraro <frankfebbraro@gmail.com>
2010-04-21 17:10:32 +0000

    Initial commit. Integration with Akamai Cache Control Utility

---
+++ akamai-page-cache-control-form.tpl.php
@@ -0,0 +1,11 @@
+<?php
+// $Id$
+/**
+ * @file akamai-page-cache-control-form.tpl
+ * Default theme implementation for displaying an akamai cache clearing 
+ * form for a site page.
+ */
+?>
+<div id="akamai-cache-control-block">
+<?php print drupal_render($cache_control_form); ?>
+</div>
