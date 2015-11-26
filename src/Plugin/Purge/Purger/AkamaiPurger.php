20bfd9d13fb2c0e40dfc81e63553110a985f0c28
Cameron Tod <cameron.tod@gmail.com>
2015-11-26 14:09:15 +0000

    Added create() method, updated constructor and client creation.

--- src/Plugin/Purge/Purger/AkamaiPurger.php
+++ src/Plugin/Purge/Purger/AkamaiPurger.php
@@ -9,7 +9,10 @@ namespace Drupal\akamai\Plugin\Purge\Purger;
 
 use Drupal\purge\Plugin\Purge\Purger\PurgerBase;
 use Drupal\purge\Plugin\Purge\Purger\PurgerInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\akamai\AkamaiClient;
+
 
 /**
  * Akamai Purger.
@@ -28,13 +31,40 @@ class AkamaiPurger extends PurgerBase implements PurgerInterface {
   // Web services client.
   protected $client;
 
-  function __construct(array $configuration, $plugin_id, $plugin_definition) {
-    $this->client = new AkamaiClient($configuration);
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('config.factory')
+    );
   }
 
   /**
-   * @inheritDoc
+   * Constructs a \Drupal\Component\Plugin\AkamaiPurger.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config
+   *   The factory for configuration objects.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ConfigFactoryInterface $config) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    $config = $config->get('akamai.config');
+    $this->client = new AkamaiClient($config);
+  }
+
+  /**
+   * {@inheritdoc}
    */
   public function getTimeHint() {
     // @todo Create a configurable max timeout.
@@ -42,15 +72,10 @@ class AkamaiPurger extends PurgerBase implements PurgerInterface {
   }
 
   /**
-   * @inheritDoc
+   * {@inheritdoc}
    */
   public function invalidate(array $invalidations) {
     // @todo: Implement invalidate() method.
-  }
-
-
-  protected function instantiateClient() {
-
   }
 
 }
