6642aa226883f7000140e4109dd542100928ccbe
Naveen Valecha <er.naveenvalecha@gmail.com>
2016-03-01 13:20:17 +0530

    Used the DI formBuilder instead from global Drupal class.

--- src/Plugin/Block/AkamaiCacheClearBlock.php
+++ src/Plugin/Block/AkamaiCacheClearBlock.php
@@ -10,6 +10,9 @@ namespace Drupal\akamai\Plugin\Block;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Form\FormBuilderInterface;
 
 /**
  * Provides a block to clear the currently viewed URL.
@@ -20,13 +23,49 @@ use Drupal\Core\Access\AccessResult;
  *   category = @Translation("Akamai")
  * )
  */
-class AkamaiCacheClearBlock extends BlockBase {
+class AkamaiCacheClearBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The Form Builder.
+   *
+   * @var \Drupal\Core\Form\FormBuilderInterface
+   */
+  protected $formBuilder;
+
+  /**
+   * Constructs a new AkamaiCacheClearBlock object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Form\FormBuilderInterface $form_builder
+   *   The Form Builder.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, FormBuilderInterface $form_builder) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->formBuilder = $form_builder;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('form_builder')
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
-    $form = \Drupal::formBuilder()->getForm('Drupal\akamai\Form\AkamaiClearUrlForm');
+    $form = $this->formBuilder->getForm('Drupal\akamai\Form\AkamaiClearUrlForm');
     return array(
       'cache_clear_form' => $form,
     );
