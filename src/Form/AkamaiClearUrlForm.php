033cf8dda1eb582045185c95bd54e4ef84c081cf
Cameron Tod <cameron.tod@gmail.com>
2016-03-02 12:57:09 +0000

    Updated placeholder format

--- src/Form/AkamaiClearUrlForm.php
+++ src/Form/AkamaiClearUrlForm.php
@@ -50,7 +50,7 @@ class AkamaiClearUrlForm extends FormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $uri_to_purge = $form_state->getValues()['path'];
     \Drupal::service('akamai.edgegridclient')->purgeUrl($uri_to_purge);
-    drupal_set_message($this->t('Asked Akamai to purge !uri', array('!uri' => $uri_to_purge)));
+    drupal_set_message($this->t('Asked Akamai to purge :uri', array(':uri' => $uri_to_purge)));
   }
 
 }
