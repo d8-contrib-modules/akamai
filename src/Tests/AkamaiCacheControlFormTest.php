7e9b2404dfcc0cdf0a690806badd2d0a65aefa1d
neetu morwani <neetumorwani@gmail.com>
2016-03-16 14:42:14 +0530

    negative urls method merged

--- src/Tests/AkamaiCacheControlFormTest.php
+++ src/Tests/AkamaiCacheControlFormTest.php
@@ -58,26 +58,20 @@ class AkamaiCacheControlFormTest extends WebTestBase {
     $edit['paths'] = 'node/1';
     $edit['domain_override'] = 'staging';
     $edit['action'] = 'invalidate';
-
     $this->drupalPostForm('admin/config/akamai/cache-clear', $edit, t('Start Refreshing Content'));
     $this->assertText(t('Requested invalidate of the following URLs: /node/1'), t('node/1 URLs purged'));
-  }
 
-  public function testInvalidUrlPurging() {
     $edit['paths'] = 'links';
     $edit['domain_override'] = 'staging';
     $edit['action'] = 'invalidate';
-
     $this->drupalPostForm('admin/config/akamai/cache-clear', $edit, t('Start Refreshing Content'));
     $this->assertText(t('Please enter at least one valid path for URL purging'), t('Invalid URL found'));
-  }
 
-  public function testExternalUrlPurging() {
     $edit['paths'] = 'https://www.google.com';
     $edit['domain_override'] = 'staging';
     $edit['action'] = 'invalidate';
-
     $this->drupalPostForm('admin/config/akamai/cache-clear', $edit, t('Start Refreshing Content'));
     $this->assertText(t('Please enter only relative paths, not full URLs'), t('External URL found '));
   }
+
 }
